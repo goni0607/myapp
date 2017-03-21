b078af3e475e50d131816f67f4c05fa4b535a34e
triplek <goni0607@gmail.com>
2017-03-21 11:59:17 +0900

    exception & debugging

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -25,7 +25,7 @@ class ArticlesController extends Controller
         $articles = \App\Article::latest()->paginate(3);
         $articles->load('user');
 
-
+        //dd(view('articles.index', compact('articles'))->render());
         return view('articles.index', compact('articles'));
     }
 
@@ -98,7 +98,11 @@ class ArticlesController extends Controller
      */
     public function show($id)
     {
-        return __METHOD__ . '은(는) 다음 기본 키를 가진 Article 모델을 조회합니다.' . $id;
+        $article = \App\Article::findOrFail($id);
+        //dd($article);
+        return $article->toArray();
+
+        //return __METHOD__ . '은(는) 다음 기본 키를 가진 Article 모델을 조회합니다.' . $id;
     }
 
     /**
